7d8e6b7da06900aed17d29735456d57f9ed4f3aa
prof-evando <profevando.borges@fiap.com.br>
2025-04-18 19:48:02 -0300

    first commit

---
+++ src/main/java/br/com/fiap/compraonline/CompraonlineApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.compraonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompraonlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompraonlineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/fiap/compraonline/CompraonlineApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.compraonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompraonlineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
