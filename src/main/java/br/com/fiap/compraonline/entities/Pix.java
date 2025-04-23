42bddd209bbc06ff1b5c290b1953f95323103978
Felipe <felipeferreira.tech@outlook.com>
2025-04-23 09:48:34 -0300

    Subindo entidades e exceção

---
+++ src/main/java/br/com/fiap/compraonline/entities/Pix.java
@@ -0,0 +1,50 @@
+package br.com.fiap.compraonline.entities;
+
+import java.util.UUID;
+
+
+public class Pix extends Pagamento {
+
+    private String codigoPagamento;
+
+    public Pix(double valor, int idCliente, int tipoPagamento) {
+        super(valor, idCliente, tipoPagamento);
+    }
+
+    @Override
+    public boolean processarPagamento() throws PagamentoException{
+
+        if (valor <= 0) {
+            throw new PagamentoException("❌ O valor do pagamento está inválido!");
+
+        } else {
+
+            System.out.println("\n🔹 Processando pagamento via Pix...");
+
+            // GERANDO CÓDIGO DO PIX
+            codigoPagamento = gerarCodigoPix();
+
+            System.out.println("\n✅ Código Pix gerado com sucesso: " + codigoPagamento);
+            System.out.println("⏳ Aguardando pagamento...");
+
+            status = "Aguardando Pagamento\n\n";
+            return true;
+
+        }
+
+    }
+
+    private String gerarCodigoPix() {
+        return "PIX-" + UUID.randomUUID().toString().substring(0, 8).toUpperCase();
+    }
+
+    public String getCodigoPagamento() {
+        return codigoPagamento;
+    }
+
+    public void setCodigoPagamento(String codigoPagamento) {
+        this.codigoPagamento = codigoPagamento;
+    }
+
+}
+
