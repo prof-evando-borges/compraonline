bbddd269cde1fc46c6a8485925098444785c5ba0
Lucas <masakinagahama2@hotmail.com>
2025-04-25 11:34:10 -0300

    falta HistoricoCompra

---
+++ src/main/java/br/com/fiap/compraonline/entities/HistoricoCompra.java
@@ -0,0 +1,11 @@
+package br.com.fiap.compraonline.entities;
+
+public class HistoricoCompra {
+    private int idUsuario;
+    private int idCompra;
+
+
+    public void visualizarCompra (int idUsuario, int idCompra){
+
+    }
+}
---
+++ src/main/java/br/com/fiap/compraonline/exceptions/CompraException.java
@@ -0,0 +1,8 @@
+package br.com.fiap.compraonline.exceptions;
+
+public class CompraException extends Exception{
+
+    public CompraException(String mensagem) {
+        super(mensagem);
+    }
+}
