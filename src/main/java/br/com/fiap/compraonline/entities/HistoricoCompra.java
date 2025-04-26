ff633cbdd3563f30039f22f7d87d5927b6bf0612
xMasaki <masakinagahama2@hotmail.com>
2025-04-26 04:59:03 -0300

    Finalizado

--- src/main/java/br/com/fiap/compraonline/entities/HistoricoCompra.java
+++ src/main/java/br/com/fiap/compraonline/entities/HistoricoCompra.java
@@ -1,11 +1,25 @@
 package br.com.fiap.compraonline.entities;
 
 public class HistoricoCompra {
+
     private int idUsuario;
-    private int idCompra;
 
-
-    public void visualizarCompra (int idUsuario, int idCompra){
-
+    public void visualizarCompras() {
+        HistoricoCompraDAO dao = new HistoricoCompraDAO();
+        dao.visualizarCompra(this);
     }
+
+    public HistoricoCompra(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
 }
+
